dc8141b1765441aa1eca84a2a386d7dbbecb067d
Artem Ryzhkov (intern) <onl1steam@gmail.com>
2020-12-19 16:18:50 +0300

    initial commit

---
+++ AvitoTraineeProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AvitoTraineeProject
+//
+//  Created by Рыжков Артем on 19.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
